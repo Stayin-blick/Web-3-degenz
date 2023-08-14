53d695391e0b3bb8749aab4793309bd13005773d
Stayin_blick <azeezbello75@gmail.com>
2023-08-14 11:50:35 +0000

    added data filters

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from web_3_degenz.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostList(generics.ListCreateAPIView):
@@ -11,7 +13,26 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count = Count('likes', distinct=True),
+        comments_count = Count('comment', distinct=True)
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    ordering_fields = [
+        'likes_count', 'comments_count', 'likes__created_at',
+    ]
+    search_fields = [
+        'owner__username', 'title'
+    ]
+    filterset_fields = [
+        'owner__profile', 'owner__followed__owner__profile', 'likes__owner__profile'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
