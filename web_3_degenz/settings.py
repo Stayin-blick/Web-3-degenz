5201412b96812ff098352a1a26c7ec9b8205ce6e
Stayin_blick <azeezbello75@gmail.com>
2023-08-17 10:44:05 +0000

    debug

--- web_3_degenz/settings.py
+++ web_3_degenz/settings.py
@@ -40,7 +40,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-stayinblick-web3degenz-w7laasnfzb4.ws-eu103.gitpod.io',
     '8000-stayinblick-web3degenz-w7laasnfzb4.ws-eu104.gitpod.io',
     os.environ.get('ALLOWED_HOST'),
     'localhost',
