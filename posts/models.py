97929b87d38debb0fb2df753eefb8c76bfec9fb7
Stayin_blick <azeezbello75@gmail.com>
2023-08-30 16:57:31 +0000

    community field

--- posts/models.py
+++ posts/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='posts')
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, default=None, blank=True, null=True, related_name='posts')
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', default='../default_post_qjpefy', blank=True)
