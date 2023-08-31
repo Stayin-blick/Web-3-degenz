d5df5997b254d30d21e7ee4672960d3311e360ce
Stayin_blick <azeezbello75@gmail.com>
2023-08-31 10:43:47 +0000

    allow the React front end to be viewed

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = 'https://web-3-degenz-f7ab1c03f599.herokuapp.com/'
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
