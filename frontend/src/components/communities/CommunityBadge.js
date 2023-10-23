c015d5ee96e4d3ffac28e98e080a0f860b90e75e
Stayin_blick <azeezbello75@gmail.com>
2023-10-23 20:00:35 +0000

    styling

--- frontend/src/components/communities/CommunityBadge.js
+++ frontend/src/components/communities/CommunityBadge.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Card } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 
 const CommunityBadge = ({ community }) => {
   return (
     <Card className="community">
       <Card.Body>
-      <Link to={`communities/<int:pk>/home_page/${community.id}`}>
-        {community.image}
-      </Link>
-        <a href={`/communities/${community.id}`}>{community.name}</a>
-        members:{community.members.length}
-        privacy:{community.privacy}
-        last visted:{community.last_visited}
+        <Container fluid>
+          <Row>
+            {/* Community image - need to fix image and links*/}
+            <Col>
+              <Link to={`/communities/${community.id}/home_page/${community.id}`}>
+                {community.image}
+              </Link>
+            </Col>
+            {/* Community info */}
+            <Col xs={6}>
+              <Card.Title>
+                <Link to={`/communities/${community.id}`}>
+                  {community.name}
+                </Link>
+              </Card.Title>
+              <Card.Subtitle>
+                members: {community.members.length}
+              </Card.Subtitle>
+              <Card.Subtitle>
+                privacy: {community.privacy}
+              </Card.Subtitle>
+              <Card.Subtitle>
+                last visited: {community.last_visited}
+              </Card.Subtitle>
+            </Col>
+          </Row>
+        </Container>
       </Card.Body>
     </Card>
   );
