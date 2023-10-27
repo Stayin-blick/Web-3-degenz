6e59598619d66e9318dcb0041b810edb1892ac8f
Stayin_blick <azeezbello75@gmail.com>
2023-10-27 11:06:56 +0000

    community badge complete

--- frontend/src/components/communities/CommunityBadge.js
+++ frontend/src/components/communities/CommunityBadge.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import { Card, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styles from '../../styles/Communities.module.css';
 
 const CommunityBadge = ({ community }) => {
+  console.log(community);
+
   return (
-    <Card className="community">
-      <Card.Body>
+    <Card className={styles.customCardSpacing}>
+      <Card.Body className="d-flex justify-content-center align-items-center">
         <Container fluid>
-          <Row>
-            {/* Community image - need to fix image and links*/}
-            <Col>
-              <Link to={`/communities/${community.id}/home_page/${community.id}`}>
-                {community.image}
+          <Row className="justify-content-center align-items-center">
+            {/* Community image */}
+            <Col xs={3} className="d-flex justify-content-center">
+              <Link to={`/communities/${community.id}/homepage/`}>
+                <img className={styles.community_pfp} src={community.image} alt='community pic' />
               </Link>
             </Col>
             {/* Community info */}
-            <Col xs={6}>
-              <Card.Title>
-                <Link to={`/communities/${community.id}`}>
-                  {community.name}
-                </Link>
-              </Card.Title>
-              <Card.Subtitle>
-                members: {community.members.length}
-              </Card.Subtitle>
-              <Card.Subtitle>
-                privacy: {community.privacy}
-              </Card.Subtitle>
-              <Card.Subtitle>
-                last visited: {community.last_visited}
-              </Card.Subtitle>
+            <Col xs={9}>
+              <div className="d-flex flex-column align-items-start">
+                <Card.Title>
+                  <Link to={`/communities/${community.id}/homepage/`}>
+                    {community.name}
+                  </Link>
+                </Card.Title>
+                <Card.Subtitle className={styles.customSubtitle}>
+                  members: {community.members.length}
+                </Card.Subtitle>
+                <Card.Subtitle className={styles.customSubtitle}>
+                  privacy: {community.privacy}
+                </Card.Subtitle>
+                <Card.Subtitle className={styles.customSubtitle}>
+                  last visited: {community.last_visited}
+                </Card.Subtitle>
+              </div>
             </Col>
           </Row>
         </Container>
